aa832362ea947440fae5cc049784305295409553
Nikolai Sjøholm Christiansen <Nikolaiviby3@gmail.com>
2018-11-23 00:58:35 +0100

    Tilføjet idle animation til begge toons. mappet er fixet.

---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private Animator _anim;
+
+    [SerializeField] private int _cooldown = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+		
+	}
+
+    public void ShootProjectile()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Debug.Log("X: "+touch.position.x);
+            Debug.Log("Y: "+touch.position.y);
+        }
+        
+        Debug.Log("PEW PEW");
+        _anim = GetComponent<Animator>();
+        _anim.SetBool("IsShooting", true);
+        Thread.Sleep(_cooldown);
+        _anim.SetBool("isShooting", false);
+    }
+}
