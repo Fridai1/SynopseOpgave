b08316cb90aef65fd6552240c47ad78b09866597
Fridai <Nikolaiviby3@gmail.com>
2018-11-27 13:14:07 +0100

    nyt shoot script

--- Assets/Script/Shoot.cs
+++ Assets/Script/Shoot.cs
@@ -10,29 +10,37 @@ public class Shoot : MonoBehaviour
     [SerializeField] private int _cooldown = 1;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	   
-		
-	}
+	using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
 
-    public void ShootProjectile()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Debug.Log("X: "+touch.position.x);
-            Debug.Log("Y: "+touch.position.y);
-        }
-        
-        Debug.Log("PEW PEW");
-        _anim = GetComponent<Animator>();
-        _anim.SetBool("IsShooting", false);
-        Thread.Sleep(_cooldown);
-        _anim.SetBool("isShooting", true);
-    }
+public class Shoot : MonoBehaviour
+{
+    private Animator _anim;
+
+    [SerializeField] private int _cooldown = 1;
+
+    // Use this for initialization
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private float bulletSpeed;
+ 
+     void Update () {
+     
+         if (Input.touchCount > 0) {
+ 
+             if (Input.GetTouch (0).phase == TouchPhase.Began) {
+ 
+                 Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                 Vector2 dir = touchPos - (new Vector2(transform.position.x, transform.position.y));
+                 dir.Normalize ();
+                 GameObject bullet = Instantiate (projectile, transform.position, Quaternion.identity);
+                 bullet.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+             }
+         }
+     }
+}
+
 }
