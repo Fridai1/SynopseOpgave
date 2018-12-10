47a7bd5c6792e8e203491af3a59a72db159c3b7c
Fridai <Nikolaiviby3@gmail.com>
2018-12-10 12:49:23 +0100

    ads

---
+++ Assets/Script/Ad.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace Assets.Script
+{
+    public class Ad : MonoBehaviour {
+
+        public void ShowAd()
+        {
+            if (Advertisement.IsReady())
+            {
+                Advertisement.Show();
+                //while (Advertisement.isShowing)
+                //{
+                //    PauseGame();
+                //}
+                //ContinueGame();
+            }
+        }
+
+        private void PauseGame()
+        {
+            Time.timeScale = 0;
+            //Disable scripts that still work while timescale is set to 0
+        }
+        private void ContinueGame()
+        {
+            Time.timeScale = 1;
+            //enable the scripts again
+        }
+    }
+
+   
+}
